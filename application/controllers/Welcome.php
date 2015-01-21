4f5b22ae51897e8d92db65c7311af3a081fbc581
Michael Chodolak <chodolak@gmail.com>
2015-01-21 11:02:24 -0800

    Updated the Welcome controller with the shucks function. Signed-off-by:Michael Chodolak <chodolak@gmail.com>

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,6 +30,21 @@ class Welcome extends Application {
 
         $this->render();
     }
+    
+    function shucks() {
+        $this->data['pagebody'] = 'justone';
+            
+        //Get the quote from the get() function
+        $quote_data = $this->quotes->get("2");
+
+        //Create an array and then assign the it from the quote_data
+        $people = array();
+        $people[] = array('who' => $quote_data['who'], 'mug' => $quote_data['mug'], 'href' => $quote_data['where'],'what' => $quote_data['what']);
+
+        //Merge the data and then render
+        $this->data = array_merge($people[0], $this->data);
+        $this->render();
+    }
 
 }
 
